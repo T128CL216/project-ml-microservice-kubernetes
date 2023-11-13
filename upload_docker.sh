2b1c258b0015f0f11f413f603baab67c9bf45f68
EC2 Default User <ec2-user@ip-172-31-6-69.ec2.internal>
2023-11-13 20:55:57 +0000

    add upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=t128cl216/project-docker:v1.0.0
 
 # Step 2:  
 # Authenticate & tag
+docker login -u "T128CL216"
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
