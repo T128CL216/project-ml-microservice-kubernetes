1e371ea871f52915994153ef1428dda847317584
EC2 Default User <ec2-user@ip-172-31-6-69.ec2.internal>
2023-11-13 20:17:12 +0000

    Add log + run_docker.sh + docker_out.txt

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import logging
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
-
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
@@ -62,9 +61,10 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f"Output prediction Value : {prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=8080, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,16 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build --tag=t128cl216/project-docker:v1.0.0 .
+sleep 20
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
-# Run flask app
+# Run flask app with docker logging
+
+docker run -it --log-driver  json-file --log-opt mode=non-blocking -p 80:80 t128cl216/project-docker:v1.0.0 
+sleep 15
+# MAP Docker port to local port 
+# docker run -p 127.0.0.1:80:9999/tcp ubuntu bash
